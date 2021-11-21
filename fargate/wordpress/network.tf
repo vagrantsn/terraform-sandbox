593034617af7a8b1546f96b19acfc4718886f7bd
Vagner S <vagners.tambalo@gmail.com>
2021-11-21 16:55:57 -0300

    feat(fargate/wordpress): add internet access to vpc

--- fargate/wordpress/network.tf
+++ fargate/wordpress/network.tf
@@ -6,6 +6,30 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_route" "main" {
+  route_table_id         = aws_vpc.main.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name" = "Wordpress"
+  }
+}
+
+resource "aws_security_group_rule" "http" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_vpc.main.default_security_group_id
+}
+
 resource "aws_subnet" "main_a" {
   vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1a"
